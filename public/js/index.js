7c8993d0b6afaa8efd307d66e595a3505725b0a4
Vincent Tripi <vinnyt95@yahoo.com>
2018-06-27 10:54:39 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket = io();
 
 socket.on("connect", function() {
   console.log("Connected to server");
-
-  socket.emit("createMessage", {
-    from: "Vincent",
-    text: "Yup, that works for me."
-  });
 });
 
 socket.on("disconnect", function() {
   console.log("Disconnected from server");
 });
 
-socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
+socket.on("newMessage", function(message) {
+  console.log("newMessage", message);
 });
