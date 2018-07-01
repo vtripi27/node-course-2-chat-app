f418d09a88c124a62b813e49e000f69cf615569d
Vincent Tripi <vinnyt95@yahoo.com>
2018-07-01 18:39:00 -0400

    Add Users class and updateUserList event

---
+++ server/utils/users.js
@@ -0,0 +1,52 @@
+[
+  {
+    id: "13214.sdf4##",
+    name: "Vincent",
+    room: "The Office Fans"
+  }
+];
+
+class Users {
+  constructor() {
+    this.users = [];
+  }
+  addUser(id, name, room) {
+    var user = { id, name, room };
+    this.users.push(user);
+    return user;
+  }
+  removeUser(id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter(user => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser(id) {
+    return this.users.filter(user => user.id === id)[0];
+  }
+  getUserList(room) {
+    var users = this.users.filter(user => user.room === room);
+    var namesArray = users.map(user => user.name);
+
+    return namesArray;
+  }
+}
+
+module.exports = { Users };
+
+// class Person {
+//     constructor (name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     getUserDescription () {
+//         return `${this.name} is ${this.age} year(s) old.`;
+//     }
+// }
+
+// var me = new Person('Vincent', 23);
+// var description = me.getUserDescription();
+// console.log(description);
