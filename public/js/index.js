9b913fa25016d02f4e43bda09855c2d355a730d8
Vincent Tripi <vinnyt95@yahoo.com>
2018-07-01 13:40:17 -0400

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,24 +9,27 @@ socket.on("disconnect", function() {
 });
 
 socket.on("newMessage", function(message) {
-  
-  var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery("<li></li>");
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var formattedTime = moment(message.createdAt).format("h:mm a");
+  var template = jQuery("#message-template").html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery("#messages").append(li);
+  jQuery("#messages").append(html);
 });
 
 socket.on("newLocationMessage", function(message) {
+  var formattedTime = moment(message.createdAt).format("h:mm a");
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery("<li></li>");
-  var a = jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr("href", message.url);
-  li.append(a);
-  jQuery("#messages").append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery("#message-form").on("submit", function(e) {
@@ -52,18 +55,18 @@ locationButton.on("click", function() {
     return alert("Geolocation not supported by your browser.");
   }
 
-  locationButton.attr("disabled", "disabled").text('Sending location...');
+  locationButton.attr("disabled", "disabled").text("Sending location...");
 
   navigator.geolocation.getCurrentPosition(
     function(position) {
-      locationButton.removeAttr("disabled").text('Send location');
+      locationButton.removeAttr("disabled").text("Send location");
       socket.emit("createLocationMessage", {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },
     function() {
-      locationButton.removeAttr("disabled").text('Send location');
+      locationButton.removeAttr("disabled").text("Send location");
       alert("Unable to fetch location.");
     }
   );
